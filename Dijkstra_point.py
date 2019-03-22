a6902750d2d1a2263ac93f348189d38362d0a71a
harsh.kakashaniya <harsh.kakashniya@gmail.com>
2019-03-22 03:56:31 -0400

    Doc:Dijkstra algorithm with point and circular robot

--- Dijkstra_point.py
+++ Dijkstra_point.py
@@ -11,27 +11,89 @@ except:
     pass
 import cv2
 
-def Map(clearance):
-    Map=255*np.ones((150,250,3)).astype(np.uint8)
+def Map(clearance,radius,resolution):
+    clearance=(clearance+radius)*resolution
+    Map=255*np.ones((int(150*resolution),int(250*resolution),3)).astype(np.uint8)
 
 
     for i in range(Map.shape[0]):
         for j in range(Map.shape[1]):
-            if(j>50-clearance and j<100+clearance and i>37-clearance and i<37+45+clearance):
+
+            #for polygon right
+
+            c1=clearance/math.sin(math.atan(-0.54)-math.pi/2)
+            c2=clearance/math.sin(math.atan(0.60)-math.pi/2)
+            c3=clearance/math.sin(math.atan(-0.18)-math.pi/2)
+
+            if(i-135*resolution-clearance<0 and j+0.54*i-245.97*resolution+c1<=0 and j-0.60*i-133.68*resolution+c2<=0 and j+0.18*i-181.05*resolution-c3>=0):
+                Map[i,j]=[0,255,0]
+
+            if(i-135*resolution-clearance<0 and i-135*resolution-0.6*clearance>=0 and j+0.54*i-245.97*resolution+c1<=0 and j+0.54*i-245.97*resolution-0.6*clearance>=0 and math.pow(i-135*resolution,2)+math.pow(j-173*resolution,2)-math.pow(clearance,2)>0):
+                Map[i,j]=[255,255,255]
+
+            if(j-0.60*i-133.68*resolution-0.6*clearance>=0 and j-0.60*i-133.68*resolution+c2<=0 and j+0.54*i-245.97*resolution+c1<=0 and j+0.54*i-245.97*resolution-0.6*clearance>=0 and math.pow(i-150*resolution+52*resolution,2)+math.pow(j-193*resolution,2)-math.pow(clearance,2)>0):
+                Map[i,j]=[255,255,255]
+
+            if(j-0.60*i-(133.68)*resolution+0.6*clearance>=0 and j-0.60*i-(133.68)*resolution+c2<=0 and j+0.18*i-(181.05)*resolution-c3>=0 and j+0.18*i-(181.05)*resolution-0.6*clearance<=0 and math.pow(i-150*resolution+90*resolution,2)+math.pow(j-170*resolution,2)-math.pow(clearance,2)>=0 ):
+                Map[i,j]=[255,255,255]
+
+
+            c4=clearance/math.sin(math.atan(-0.18)-math.pi/2)
+            c5=clearance/math.sin(math.atan(9.5)-math.pi/2)
+            c6=clearance/math.sin(math.atan(0.6)-math.pi/2)
+
+            #for polygon left
+            if(i-135*resolution-clearance<0 and j+0.18*i-181.05*resolution+c4<=0 and j-9.5*i+768.0*resolution+c5<=0 and j-0.6*i-67.68*resolution-c6>=0):
+                Map[i,j]=[0,255,0]
+
+            if(i-135*resolution-clearance-0.2*clearance<0 and i-(135)*resolution-0.5*clearance>=0 and j-0.6*i-67.68*resolution-c6+0.2*clearance>=0 and j-0.6*i-(67.68)*resolution+0.3*clearance<=0 and math.pow(i-135*resolution,2)+math.pow(j-150*resolution,2)-math.pow(clearance,2)>=0):
+                Map[i,j]=[255,255,255]
+
+            if(j-9.5*i+(768.0)*resolution+c5<=0 and j-9.5*i+(768.0)*resolution+5*clearance>=0 and j-0.6*i-(67.68)*resolution+0.6*clearance-c6>=0 and j-0.6*i-(67.68)*resolution-0.6*clearance<=0 and math.pow(i-150*resolution+56*resolution,2)+math.pow(j-125*resolution,2)-math.pow(clearance,2)>=0):
+                Map[i,j]=[255,255,255]
+
+            #for rectrangle
+            if(j>50*resolution-clearance and j<100*resolution+clearance and i>37.5*resolution-clearance and i<37.5*resolution+45*resolution+clearance):
+                Map[i,j]=[0,255,0]
+
+            if(j>=50*resolution-clearance and j<=50*resolution and i>=37.5*resolution-clearance and i<=37.5*resolution and math.pow(i-37.5*resolution,2)+math.pow(j-50*resolution,2)-math.pow(clearance,2)>0):
+                Map[i,j]=[255,255,255]
+
+            if(j>=50*resolution-clearance and j<=50*resolution and i<=37.5*resolution+45*resolution+clearance and i>=37.5*resolution+45*resolution and math.pow(i-37.5*resolution-45*resolution,2)+math.pow(j-50*resolution,2)-math.pow(clearance,2)>0):
+                Map[i,j]=[255,255,255]
+
+            if(j<=100*resolution+clearance and j>=100*resolution and i<=37.5*resolution+45*resolution+clearance and i>=37.5*resolution+45*resolution and math.pow(i-37.5*resolution-45*resolution,2)+math.pow(j-100*resolution,2)-math.pow(clearance,2)>0 and radius<14 ):
+                Map[i,j]=[255,255,255]
+
+            if(j<=100*resolution+clearance and j>=100*resolution and i>=37.5*resolution-clearance and i<=37.5*resolution and math.pow(i-37.5*resolution,2)+math.pow(j-100*resolution,2)-math.pow(clearance,2)>0):
+                Map[i,j]=[255,255,255]
+
+            #for circle
+            if((math.pow(i-20*resolution,2)+math.pow(j-190*resolution,2))-math.pow(15*resolution+clearance,2)<=0):
+                Map[i,j]=[0,255,0]
+
+            #for ellipse
+            if((math.pow(i-30*resolution,2)/math.pow(6*resolution+clearance,2))+math.pow(j-140*resolution,2)/math.pow(15*resolution+clearance,2)<=1):
+                Map[i,j]=[0,255,0]
+
+    for i in range(Map.shape[0]):
+        for j in range(Map.shape[1]):
+            if(j>50*resolution and j<100*resolution and i>37.5*resolution and i<37.5*resolution+45*resolution):
                 Map[i,j]=[0,0,255]
 
-            if((math.pow(i-20,2)+math.pow(j-190,2))<math.pow(15+clearance,2)):
+            if((math.pow(i-20*resolution,2)+math.pow(j-190*resolution,2))<math.pow(15*resolution,2)):
                 Map[i,j]=[0,0,255]
 
-            if((math.pow(i-30,2)/math.pow(6+clearance,2))+math.pow(j-140,2)/math.pow(15+clearance,2)<1):
+            if((math.pow(i-30*resolution,2)/math.pow(6*resolution,2))+math.pow(j-140*resolution,2)/math.pow(15*resolution,2)<1):
                 Map[i,j]=[0,0,255]
 
-            if(i-135-clearance<0 and j+0.54*i-245.97<0 and j-0.60*i-133.68<0 and j+0.18*i-181.05>0):
+            if(i-135*resolution<0 and j+0.54*i-245.97*resolution<0 and j-0.60*i-133.68*resolution<0 and j+0.18*i-181.05*resolution>0):
                 Map[i,j]=[0,0,255]
 
-            if(i-135<0 and j+0.18*i-181.05<0 and j-9.5*i+768.0<0 and j-0.6*i-67.68>0):
+            if(i-135*resolution<0 and j+0.18*i-181.05*resolution<0 and j-9.5*i+768.0*resolution<0 and j-0.6*i-67.68*resolution>0):
                 Map[i,j]=[0,0,255]
     return Map
+
 
 def exp_right(Map,explorer,cost,x,y):
     if(y+1<250 and Map[x,y+1,0]==255):
@@ -151,11 +213,12 @@ def video(img_array):
     video.release()
 
 #Point_free_space_old=Map(10)
-Point_free_space=Map(0)
+Point_free_space=Map(0,5,1)
+print("Map is ready")
 explorer=10000*np.array(np.ones((150,250)),dtype=float)
 cost=10000*np.array(np.ones((150,250,3)),dtype=float)
-Startx=1
-Starty=1
+Startx=100
+Starty=50
 explorer[Startx,Starty]=0
 cost[Startx,Starty]=0
 Endx=149
@@ -174,12 +237,13 @@ while(np.min(explorer)<5000 and error==0):
     least=np.argmin(explorer)
     exp_x=int(least/250)
     exp_y=least%250
-    print(exp_x)
-    print(exp_y)
+    #print(exp_x)
+    #print(exp_y)
     cost,explorer,Point_free_space=pointexplore(Point_free_space,explorer,cost,exp_x,exp_y)
     invideo=Point_free_space.copy()
     img_array.append(invideo)
-    #count += 1
+    count += 1
+    print(count)
     #cv2.imwrite('%d.jpg' %count,Point_free_space)
 
 
